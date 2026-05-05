54b88b45ade6c2d92565017164cd3915fe7d1317
firedroon-hash <firedroon@gmail.com>
2026-05-05 13:23:03 +0800

    Delete Program_F_Strategy.py

--- Program_F_Strategy.py
+++
@@ -1,9 +0,0 @@
-def enhanced_process(prices, context=None):
-    # 強制回傳 BUY，測試 Discord 與 PDF
-    return {
-        "action": "BUY",
-        "entry": prices[-1],
-        "stop_loss": prices[-1] * 0.97,
-        "pred_high": prices[-1] * 1.05,
-        "info": "測試發送中"
-    }
